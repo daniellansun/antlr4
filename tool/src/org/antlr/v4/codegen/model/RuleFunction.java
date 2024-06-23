3a5dbd5c9e1bc8d0564d8bae2c65e02d2c85a411
Daniel Sun <sunlan@apache.org>
2024-06-23 09:24:59 +0900

    Trivial refactoring: use `Collection.removeIf()`

--- tool/src/org/antlr/v4/codegen/model/RuleFunction.java
+++ tool/src/org/antlr/v4/codegen/model/RuleFunction.java
@@ -256,11 +256,7 @@ public class RuleFunction extends OutputModelObject {
 				}
 			}
 
-			for (String ref : nonOptional.toArray(new String[0])) {
-				if (minFreq.count(ref) == 0) {
-					nonOptional.remove(ref);
-				}
-			}
+			nonOptional.removeIf(ref -> minFreq.count(ref) == 0);
 
 			firstAlt = false;
 		}
